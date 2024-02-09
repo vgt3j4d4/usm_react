69bedf4bdb14ed44d85614ffa76df6e545218c67
vtejada <vgtejada@gmail.com>
2024-02-09 08:45:26 -0500

    - adding changes for netlify to build

--- src/context/MapSelectionContext.js
+++ src/context/MapSelectionContext.js
@@ -1,22 +1,22 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 export const MapSelectionContext = createContext();
 
 export default function MapSelectionProvider({ children }) {
   const [selected, setSelected] = useState({});
 
+  const focus = useCallback(() => {
+    if (!selected.id) return;
+    const note = document.querySelector(`[data-note-id="${selected.id}"]`);
+    if (note) note.focus();
+  }, [selected]);
+
   useEffect(() => {
     if (selected.id) {
       const needFocus = !!selected.focus;
       if (needFocus) focus();
     }
-  }, [selected]); // TODO: React Hook useEffect has a missing dependency: 'focus'
-
-  function focus() {
-    if (!selected.id) return;
-    const note = document.querySelector(`[data-note-id="${selected.id}"]`);
-    if (note) note.focus();
-  }
+  }, [selected, focus]); // TODO: React Hook useEffect has a missing dependency: 'focus'
 
   return (
     <MapSelectionContext.Provider value={{
