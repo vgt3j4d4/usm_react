fe7b0962d77cc65c21af35898b5d03cb75f4fede
vtejada <vgtejada@gmail.com>
2024-02-09 19:22:36 -0500

    -  moving DEFAULT_STORY, DEFAULT_FEATURE, DEFAULT_EPIC constants to StoriesService.js - moving buildEpic, buildFeature, buildStory functions to StoriesService.js -  adding localStorage persistance

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,36 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
-import { DEFAULT_EPIC, DEFAULT_FEATURE, DEFAULT_STORY } from "../const";
 
-function id() {
+export function id() {
   return uuidv4().split('-').join('');
-}
-
-export function buildEpic() {
-  const epicId = id();
-  return {
-    id: epicId,
-    ...DEFAULT_EPIC,
-    features: [buildFeature(epicId)]
-  }
-}
-
-export function buildFeature(epicId) {
-  const featureId = id();
-  return {
-    id: featureId,
-    epicId: epicId,
-    ...DEFAULT_FEATURE,
-    stories: [buildStory(featureId)]
-  }
-}
-
-export function buildStory(featureId) {
-  const storyId = id();
-  return {
-    id: storyId,
-    featureId: featureId,
-    ...DEFAULT_STORY
-  };
 }
 
 export function getFeatures(epics = [], features = []) {
