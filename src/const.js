74efc489e140a63e180f18f089f4249f3c3a7325
vtejada <vgtejada@gmail.com>
2024-02-09 19:22:36 -0500

    -  moving DEFAULT_STORY, DEFAULT_FEATURE, DEFAULT_EPIC constants to StoriesService.js - moving buildEpic, buildFeature, buildStory functions to StoriesService.js -  adding localStorage persistance

--- src/const.js
+++ src/const.js
@@ -4,20 +4,6 @@ export const NOTE_TYPE = Object.freeze({
   EPIC: 'EPIC',
   FEATURE: 'FEATURE',
   STORY: 'STORY'
-})
-
-export const DEFAULT_STORY = Object.freeze({
-  title: 'New Story'
-});
-
-export const DEFAULT_FEATURE = Object.freeze({
-  title: 'New Feature',
-  stories: [{ ...DEFAULT_STORY }]
-});
-
-export const DEFAULT_EPIC = Object.freeze({
-  title: 'New Epic',
-  features: [{ ...DEFAULT_FEATURE }]
 });
 
 export const ARROW_KEYS = Object.freeze({
--- src/services/StoriesService.js
+++ src/services/StoriesService.js
@@ -1,64 +1,56 @@
-import { buildEpic, buildFeature, buildStory, clone } from "../utils/utils";
+import { id, clone } from "../utils/utils";
 
-const storyMap = {
-  id: null,
-  epics: [buildEpic()]
-};
-const prefixes = {
-  epics: 1,
-  features: 1,
-  stories: 1
-};
+const LOCAL_STORAGE_KEY = 'STORY_MAP';
+
+const DEFAULT_STORY = Object.freeze({ title: 'New Story' });
+const DEFAULT_FEATURE = Object.freeze({ title: 'New Feature', stories: [{ ...DEFAULT_STORY }] });
+const DEFAULT_EPIC = Object.freeze({ title: 'New Epic', features: [{ ...DEFAULT_FEATURE }] });
+
+const _storyMap = { id: null, epics: [buildEpic()] };
 
 export async function getStoryMap(_storyMapId) {
+  let storyMap = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  if (storyMap) {
+    storyMap = JSON.parse(storyMap);
+    _storyMap.epics = storyMap.epics;
+  } else {
+    storyMap = { ..._storyMap, id: _storyMapId || null };
+  }
+
   return Promise.resolve(clone(storyMap));
 }
 
 export async function addEpic(_storyMapId) {
   const epic = buildEpic();
-
-  const { epics } = storyMap;
-  prefixes.epics += 1;
-  epic.title += ` ${prefixes.epics}`;
-  prefixes.features += 1;
-  const feature = epic.features[epic.features.length - 1];
-  feature.title += ` ${prefixes.features}`;
-  prefixes.stories += 1;
-  const story = feature.stories[feature.stories.length - 1];
-  story.title += ` ${prefixes.stories}`;
-
+  const { epics } = _storyMap;
   epics.push(epic);
+  save();
+
   return Promise.resolve(clone(epic));
 }
 
 export async function addFeature(_storyMapId, epicId) {
-  const epic = storyMap.epics.find(e => e.id === epicId);
+  const epic = _storyMap.epics.find(e => e.id === epicId);
   if (epic) {
     const feature = buildFeature(epicId);
-
-    prefixes.features += 1;
-    feature.title += ` ${prefixes.features}`;
-    prefixes.stories += 1;
-    const story = feature.stories[feature.stories.length - 1];
-    story.title += ` ${prefixes.stories}`;
-
     epic.features.push(feature);
+    save();
+
     return Promise.resolve(clone(feature));
   }
   return Promise.resolve(null);
 }
 
 export async function addStory(_storyMapId, epicId, featureId) {
-  const epic = storyMap.epics.find(e => e.id === epicId);
+  const epic = _storyMap.epics.find(e => e.id === epicId);
   if (epic) {
     const feature = epic.features.find(f => f.id === featureId);
     if (feature) {
       const story = buildStory(featureId);
-
-      prefixes.stories += 1;
-      story.title += ` ${prefixes.stories}`;
-
       feature.stories.push(story);
+      save();
+
       return Promise.resolve(clone(story));
     }
   }
@@ -66,20 +58,22 @@ export async function addStory(_storyMapId, epicId, featureId) {
 }
 
 export async function removeEpic(epicId) {
-  const epic = storyMap.epics.find(e => e.id === epicId);
+  const epic = _storyMap.epics.find(e => e.id === epicId);
   if (epic) {
-    storyMap.epics = storyMap.epics.filter(e => e.id !== epicId);
+    _storyMap.epics = _storyMap.epics.filter(e => e.id !== epicId);
+    save();
     return Promise.resolve(epic);
   }
   return Promise.resolve(null);
 }
 
 export async function removeFeature(epicId, featureId) {
-  const epic = storyMap.epics.find(e => e.id === epicId);
+  const epic = _storyMap.epics.find(e => e.id === epicId);
   if (epic) {
     const feature = epic.features.find(f => featureId);
     if (feature) {
       epic.features = epic.features.filter(f => f.id !== featureId);
+      save();
       return Promise.resolve(feature);
     }
   }
@@ -87,16 +81,49 @@ export async function removeFeature(epicId, featureId) {
 }
 
 export async function removeStory(epicId, featureId, storyId) {
-  const epic = storyMap.epics.find(e => e.id === epicId);
+  const epic = _storyMap.epics.find(e => e.id === epicId);
   if (epic) {
     const feature = epic.features.find(f => featureId);
     if (feature) {
       const story = feature.stories.find(s => s.id === storyId);
       if (story) {
         feature.stories = feature.stories.filter(s => s.id !== storyId);
+        save();
         return Promise.resolve(story);
       }
     }
   }
   return Promise.resolve(null);
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(_storyMap));
+}
+
+function buildEpic() {
+  const epicId = id();
+  return {
+    id: epicId,
+    ...DEFAULT_EPIC,
+    features: [buildFeature(epicId)]
+  }
+}
+
+function buildFeature(epicId) {
+  const featureId = id();
+  return {
+    id: featureId,
+    epicId: epicId,
+    ...DEFAULT_FEATURE,
+    stories: [buildStory(featureId)]
+  }
+}
+
+function buildStory(featureId) {
+  const storyId = id();
+  return {
+    id: storyId,
+    featureId: featureId,
+    ...DEFAULT_STORY
+  };
 }
