3f85a8a0c0d698ec3353e632e2388535530e2b71
vtejada <vgtejada@gmail.com>
2024-02-11 12:40:45 -0500

    - removing the <button> from the NavButtom element and replace it with <span>

--- src/components/Toolbar/NavButton.jsx
+++ src/components/Toolbar/NavButton.jsx
@@ -2,20 +2,24 @@ import { Link } from "react-router-dom";
 
 export default function NavButton({ active, position, label, icon, route }) {
   const className = [
-    'px-4 py-2 text-sm font-medium focus:ring-2',
+    'inline-block px-4 py-2 text-sm font-medium focus:ring-2',
     active ? 'bg-white text-black' : 'border border-gray-500 hover:bg-gray-300 hover:text-gray-900',
     position === 'first' ? 'rounded-s-lg' : position === 'last' ? 'rounded-e-lg' : ''
   ].join(' ').trim();
 
+  if (active) return (
+    <span className={className}>
+      <i className={`fa-solid ${icon} mr-1`}></i>
+      <span className="max-md:hidden">{label}</span>
+    </span>
+  );
+
   return (
     <Link to={route}>
-      <button type="button"
-        className={className}
-        aria-pressed={active}
-        tabIndex={active ? -1 : 0}>
+      <span className={className}>
         <i className={`fa-solid ${icon} mr-1`}></i>
         <span className="max-md:hidden">{label}</span>
-      </button>
+      </span>
     </Link>
   )
 }
