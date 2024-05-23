b7cd76d252d24f22309e353ba15c7372cc01f0b1
vgt3j4d4 <vgt3j4d4@gmail.com>
2024-05-22 20:50:47 -0500

    refactor: update NOTE_TYPE values to use numeric representation

--- src/const.js
+++ src/const.js
@@ -1,10 +1,6 @@
 export const VERSION = '1.0.0';
 
-export const NOTE_TYPE = Object.freeze({
-  EPIC: 'EPIC',
-  FEATURE: 'FEATURE',
-  STORY: 'STORY'
-});
+export const NOTE_TYPE = Object.freeze({ EPIC: 0, FEATURE: 1, STORY: 2 });
 
 export const ARROW_KEYS = Object.freeze({
   UP: 'ArrowUp',
